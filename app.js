8767ad89b6a47fcaa09df26d7c5829016f9c4aa6
Ebin Benny Thomas <ebinbthomas@gmail.com>
2021-10-22 18:55:52 +0530

    completed to host

--- app.js
+++ app.js
@@ -54,16 +54,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-
-
-
-
-// app.use(function (req, res, next) {
-//   //Enabling CORS
-//   res.header("Access-Control-Allow-Origin", "*");
-//   res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type,Accept, x-client-key, x-client-token, x-client-secret, Authorization");
-//     next();
-//   });
-// catch 404 and forward to error handler
